93b81b0d5957659f143eb926da8043a48d991339
Tiago Almeida <tyagow@hotmail.com.br>
2016-10-24 17:58:07 -0200

    Criando v0.2 com classes separadas em arquivos

---
+++ v0.2/base.py
@@ -0,0 +1,49 @@
+import pygame
+from pygame.locals import *
+
+FPS = 60
+WIDTH = 800
+HEIGHT = 600
+
+class Base_state(object):
+    done = False
+    clock = ""
+    manager = None
+
+    def __init__(self, width=WIDTH, height=HEIGHT):
+        pygame.init()
+        self.width, self.height = width, height
+        self.screen = pygame.display.set_mode((width, height))
+        self.clock = pygame.time.Clock()
+        self.clock.tick()
+        self.screen.fill(Color("white"))
+
+    def main_loop(self):
+        while not self.done:
+            # events
+            events = pygame.event.get()
+            for e in events:
+                if e.type == pygame.QUIT: self.done = True
+                elif e.type == KEYDOWN:
+                    if e.key == K_ESCAPE: self.done = True
+                else: self.handle_input(e)
+
+            # update
+            self.clock.tick(FPS)
+            self.update(self.clock)
+
+            # draw
+            self.screen.fill((255,255,255))
+            self.draw(self.screen)
+            pygame.display.update()
+
+    def handle_inputs(self, events):
+        pass
+
+    def update(self, clock):
+        pass
+
+    def draw(self, screen):
+        pass
+
+
---
+++ v0.2/manager.py
@@ -0,0 +1,60 @@
+from random import randint
+
+import base
+import constantes
+from Pessoa import Pessoa
+from base import Base_state
+from pygame.locals import *
+
+
+MAX_PESSOAS = 150
+DRAW_INFLUENCIA = False
+
+
+class GameManager(Base_state):
+
+    def __init__(self):
+        super().__init__()
+        self.carrega_pessoas()
+
+    def update(self, clock):
+        for pessoa in constantes.pessoas:
+            pessoa.update(clock.get_time())
+        # print(clock.get_time())
+
+    def draw(self, screen):
+        for pessoa in constantes.pessoas:
+            pessoa.draw(screen)
+
+    def handle_input(self, event):
+        if event.type == QUIT:
+            self.done = True
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                self.done = True
+        elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+            for p in constantes.pessoas:
+                p.event_click(event)
+
+    def carrega_pessoas(self, total_pessoas=MAX_PESSOAS):
+        for i in range(total_pessoas):
+            rand_x = randint(20, base.WIDTH - 20)
+            rand_y = randint(20, base.HEIGHT - 20)
+            rand_sentimento = randint(-1, 1)
+            rand_peso = randint(10, 25)
+            influencia = rand_peso * 3
+
+            pessoa = Pessoa(rand_x, rand_y, rand_sentimento, influencia, rand_peso)
+
+            while pessoa.colide(constantes.pessoas):
+                rand_x = randint(30, base.WIDTH - 30)
+                rand_y = randint(30, base.HEIGHT - 30)
+                pessoa.x = rand_x
+                pessoa.y = rand_y
+
+            constantes.pessoas.append(pessoa)
+
+
+if __name__ == "__main__":
+    g = GameManager()
+    g.main_loop()
